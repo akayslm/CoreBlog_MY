86a2ea23ab2ad6661d675de34749e2387941fb3f
akayslm <73195655+akayslm@users.noreply.github.com>
2023-01-09 23:46:47 +0300

    Yazarın Diğer Yazıları Ve Yazar Blog İlişkisi

---
+++ CoreDemo_MY/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo_MY.ViewComponents.Blog
+{
+    public class WriterLastBlog:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
+    }
+}
