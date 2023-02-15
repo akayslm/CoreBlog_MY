cd23e706119c9f7937181f380249d2e8a43f2c6e
akayslm <73195655+akayslm@users.noreply.github.com>
2023-02-15 18:42:37 +0300

    AboutController kısmının kalan işleri tamamlandı

--- CoreDemo_MY/Controllers/AboutController.cs
+++ CoreDemo_MY/Controllers/AboutController.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EF;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo_MY.Controllers
 {
     public class AboutController : Controller
     {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = abm.GetList();
+            return View(values);
         }
 
         public PartialViewResult SocialMediaAbout()
-        {
+        {            
             return PartialView();
         }
     }
