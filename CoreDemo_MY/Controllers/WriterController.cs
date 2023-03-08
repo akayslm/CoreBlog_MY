8a3956ecdc815699f8dc53e2b42f0afe0e7e597e
akayslm <73195655+akayslm@users.noreply.github.com>
2023-03-08 19:32:00 +0300

    Yazar Navbar Düzenlemeleri yapıldı

--- CoreDemo_MY/Controllers/WriterController.cs
+++ CoreDemo_MY/Controllers/WriterController.cs
@@ -17,9 +17,20 @@ namespace CoreDemo_MY.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
         public IActionResult Test()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
         }
     }
 }
